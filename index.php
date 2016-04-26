15a2422c2bb6e7a557ca60a1b2127cd4fb82ce4a
avezhlev <avezhlev@gmail.com>
2016-04-26 15:33:45 +0500

    ui update

--- index.php
+++ index.php
@@ -13,18 +13,31 @@
 			<input name='userfile' type='file' /><br />
 			
 			<div class='wrapper small'>
-			<div class='table'><div class='row'>
+				<div class='table'>
+					<div class='row header small blue'>
+						<div class='cell small'>
+							Objects, groups
+						</div>
+						<div class='cell small'>
+							<input type='checkbox' id='nat' name='nat' checked>
+							<label for='nat'>NAT rule</label>
+						</div>
+						<div class='cell small'>
+							<input type='checkbox' id='acl' name='acl' checked>
+							<label for='acl'>ACL</label>
+						</div>
+					</div>
 			
-			<div class='cell small'><input type='checkbox' id='empty' name='empty'>
-			<label for='empty'>Show empty</label></div>
+					<div class='row'>
+						<div class='cell small'>
+							<input type='checkbox' id='empty' name='empty'>
+							<label for='empty'>Show empty</label>
+						</div>
 			
-			<div class='cell small'><input type='checkbox' id='nat' name='nat' checked>
-			<label for='nat'>NAT</label></div>
+					</div>
+				</div>
+			</div>
 			
-			<div class='cell small'><input type='checkbox' id='acl' name='acl' checked>
-			<label for='acl'>ACL</label></div>
-			
-			</div></div></div>
 			<input type='submit' value='Parse' />
 		</form>
 	</div>
